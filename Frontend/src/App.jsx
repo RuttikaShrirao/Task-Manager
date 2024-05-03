1eec67484770e8532e684a17c9a135505f0f8f37
RuttikaShrirao <ruttikashrirao@gmail.com>
2024-05-03 23:57:00 +0530

    redux bug fixed.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { Routes, Route,  Outlet,
-  Navigate } from "react-router-dom";
-import Login from "./pages/Login";
+
 import Registration from "./pages/Registration";
 import Home from "./pages/Home";
 import {TOKEN_KEY} from './utils/constants'
+// --------------------------------------------------------------------
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "sonner";
+// import Navbar from "./components/Navbar";
+// import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
+import TaskDetails from "./pages/TaskDetails";
+import Tasks from "./pages/Tasks";
+import Trash from "./pages/Trash";
+import Users from "./pages/Users";
+// import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
 
@@ -16,7 +25,29 @@ function App() {
           <Route path="/" element={<Home />} />
         </Route>
         <Route path="*" element={"404"} />
+      </Routes> */}
+
+{/* ------------------------------------------------------------------------------------------ */}
+      <main className='w-full min-h-screen bg-[#f3f4f6] '>
+      <Routes>
+        {/* <Route element={<Layout />}> */}
+          <Route index path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users />} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        {/* </Route> */}
+
+        <Route path='/log-in' element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
       </Routes>
+
+      <Toaster richColors />
+    </main>
     </>
   );
 }
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { useState,useEffect } from "react";
-import { json, useNavigate } from "react-router-dom";
+// import * as React from 'react';
+import { useState } from "react";
+import { json } from "react-router-dom";
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
@@ -13,6 +13,15 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { API_URL, TOKEN_KEY} from '../utils/constants'
 import "../App.css";
 import '../index.css'
+// ---------------------------------------------------------------------------------------------
+
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
+// import { useSelector } from "react-redux";
+
 
 export default function Login() {
   const[loginInfo, setLoginInfo] = useState({
@@ -67,42 +76,140 @@ const handleClickShowPassword = () => setShowPassword((show) => !show);
       setLoginInfo({...loginInfo,password:(e.target.value)})
     //   console.log({...loginInfo,password:(e.target.value)})
     }
+// ---------------------------------------------------------------------------------------
+// const { user } = useSelector((state) => state.auth);
+const { user } = ""
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+} = useForm();
 
-  return (
-  <div className='form'>
-  <FormControl sx={{ mt: 2, mb: 4, width: '25ch' }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-password">Username</InputLabel>
-          <Input
-            id="standard-adornment-password"
-            type="text" onChange={loginEmail}
+console.log(  register,"pppp",handleSubmit);
+// const navigate = useNavigate();
 
-          />
-        </FormControl>
-      <FormControl sx={{ mt: 2, mb: 4, width: '25ch' }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input
-            onChange={loginPassword}
-            id="standard-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                //   onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
+const submitHandler = async (data) => {
+  console.log("submit",data);
+};
 
-      <Button variant="contained" onClick={loginHandler}>Login</Button>
-      <p>{msg}</p>
-     <p>Don't have account? </p>
-     <a onClick={()=>{navigate('/registration')}}>sign up</a>
+useEffect(() => {
+  user && navigate("/dashboard");
+}, [user]);
+
+
+  // return (
+  // <div className='form'>
+  // <FormControl sx={{ mt: 2, mb: 4, width: '25ch' }} variant="standard">
+  //         <InputLabel htmlFor="standard-adornment-password">Username</InputLabel>
+  //         <Input
+  //           id="standard-adornment-password"
+  //           type="text" onChange={loginEmail}
+
+  //         />
+  //       </FormControl>
+  //     <FormControl sx={{ mt: 2, mb: 4, width: '25ch' }} variant="standard">
+  //         <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+  //         <Input
+  //           onChange={loginPassword}
+  //           id="standard-adornment-password"
+  //           type={showPassword ? 'text' : 'password'}
+  //           endAdornment={
+  //             <InputAdornment position="end">
+  //               <IconButton
+  //                 aria-label="toggle password visibility"
+  //                 onClick={handleClickShowPassword}
+  //               //   onMouseDown={handleMouseDownPassword}
+  //               >
+  //                 {showPassword ? <VisibilityOff /> : <Visibility />}
+  //               </IconButton>
+  //             </InputAdornment>
+  //           }
+  //         />
+  //       </FormControl>
+
+  //     <Button variant="contained" onClick={loginHandler}>Login</Button>
+  //     <p>{msg}</p>
+  //    <p>Don't have account? </p>
+  //    <a onClick={()=>{navigate('/registration')}}>sign up</a>
+  //   </div>
+  // );
+// -----------------------------------------------------------------------------------------------
+
+return (
+  <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6] '>
+    <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+      {/* left side */}
+      <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+        <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+          <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
+            Manage all your task in one place!
+          </span>
+          <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+            <span>Cloud-Based</span>
+            <span>Task Manager</span>
+          </p>
+
+          <div className='cell'>
+            <div className='circle rotate-in-up-left'></div>
+          </div>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+        <form
+          onSubmit={handleSubmit(submitHandler)}
+          className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
+        >
+          <div className=''>
+            <p className='text-blue-600 text-3xl font-bold text-center'>
+              Welcome back!
+            </p>
+            <p className='text-center text-base text-gray-700 '>
+              Keep all your credential safe.
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-y-5'>
+            <Textbox
+              placeholder='email@example.com'
+              type='email'
+              name='email'
+              label='Email Address'
+              className='w-full rounded-full'
+              register={register("email", {
+                required: "Email Address is required!",
+              })}
+              error={errors.email ? errors.email.message : ""}
+            />
+            <Textbox
+              placeholder='your password'
+              type='password'
+              name='password'
+              label='Password'
+              className='w-full rounded-full'
+              register={register("password", {
+                required: "Password is required!",
+              })}
+              error={errors.password ? errors.password.message : ""}
+            />
+
+            <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+              Forget Password?
+            </span>
+
+            <Button
+              type='submit'
+              label='Submit'
+              className='w-full h-10 bg-blue-700 text-white rounded-full'
+            />
+          </div>
+        </form>
+      </div>
     </div>
-  );
+  </div>
+);
+
+
 }
   
